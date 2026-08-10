670af4742bec96833007d1b44286beef86d78860
MrugankKapse-test <mrugank.kapse@corecotechnologies.com>
2026-08-10 14:31:01 +0530

    Change screenshot path in pop_up.py
    Updated screenshot path for 'No Records Found' case.

--- tests/pop_up.py
+++ tests/pop_up.py
@@ -51,18 +51,12 @@ with sync_playwright() as p:
 
         # Capture screenshot
         page.screenshot(
-            path="/Users/mrugankkapse/Documents/Playwright_Project/test/no_records_found.png",
+            path="/Users/mrugankkapse/Documents/Playwright_Project/test/leave_pop_up.png",
             full_page=True
         )
 
         print("No Records Found message detected")
         print("Screenshot saved")
-
-    else:
-
-        print("Records are available")
-
     page.wait_for_timeout(3000)
-
     # 10. Close browser
     browser.close()
